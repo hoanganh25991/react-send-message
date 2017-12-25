a8a5b8e54419200080484aeff09ac24995413dce
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-12-25 11:06:24 +0700

    Update style & homepage

--- src/components/App/style.js
+++ src/components/App/style.js
@@ -1,27 +1,30 @@
 const _20px = 20
+const _10px = 10
 const curveRadius = 5
 
 export const style = {
   rootDiv: {
-    border: "1px solid black",
-    borderRadius: curveRadius,
-    padding: _20px
+    // border: "1px solid black",
+    // borderRadius: curveRadius,
+    // padding: _10px,
   },
   sendMsgDiv: {
     width: 250,
     minWidth: 320,
   },
   title: {
-    fontSize: "1.6em",
+    fontSize: 22,
     fontWeight: "bold",
-    textAlign: "center"
+    textAlign: "center",
+    marginBottom: _10px,
   },
   sendMsgDivHasData: {
     display: "flex",
     width: "75vw",
-    height: "80vh",
+    // height: "95vh",
     overflowY: "auto",
     flexDirection: "column",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    overflowX: "hidden",
   }
 }
