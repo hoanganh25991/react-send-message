d7a4e4ada9b0a41c0f22b2037286ae48f6bee25d
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-12-25 17:36:49 +0700

    Fix bug publish

--- src/components/SendMessage/index.js
+++ src/components/SendMessage/index.js
@@ -2,15 +2,15 @@ import React, { PureComponent } from "react"
 import { style as s } from "./style"
 import * as firebase from "firebase"
 import CustomerInfo from "../CustomerInfo"
-import {sendMsg} from "../../facebook-api/pageSendMsg";
-import {publishPost} from "../../facebook-api/pagePublishPost";
+import { sendMsg } from "../../facebook-api/pageSendMsg"
+import { publishPost } from "../../facebook-api/pagePublishPost"
 
 const _ = console.log
 
 const firebaseConfig = {
   apiKey: "AIzaSyDXbD71Y_uA8ldm1h9S2-6AOgl73UOid1U",
   authDomain: "glass-turbine-148103.firebaseapp.com",
-  databaseURL: "https://glass-turbine-148103.firebaseio.com",
+  databaseURL: "https://glass-turbine-148103.firebaseio.com"
 }
 
 const mainBranch = "tmp/sendMessageDemo/pages"
@@ -25,79 +25,82 @@ export default class SendMessage extends PureComponent {
     postMsg: "",
     isPosting: false,
     postUrl: "",
-    postImgFile: null,
+    postImgFile: ""
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getOptInCustomers()
   }
 
-  componentWillUnmount(){
-    const {fbApp} = this.state
-    if(!fbApp) return
-    fbApp.close().then(() => _("[fbApp] closed")).catch(err => _("[fbApp] [ERR]", err))
+  componentWillUnmount() {
+    const { fbApp } = this.state
+    if (!fbApp) return
+    fbApp
+      .close()
+      .then(() => _("[fbApp] closed"))
+      .catch(err => _("[fbApp] [ERR]", err))
   }
 
   getOptInCustomers = () => {
-    const {page : {id: pageId}} = this.props
-    if(!pageId) return
-    const fbApp =firebase.initializeApp(firebaseConfig, pageId);
-    const db = fbApp.database();
-    this.setState({db, fbApp}, () => {
+    const { page: { id: pageId } } = this.props
+    if (!pageId) return
+    const fbApp = firebase.initializeApp(firebaseConfig, pageId)
+    const db = fbApp.database()
+    this.setState({ db, fbApp }, () => {
       this.storePageToken()
     })
 
     db.ref(`${mainBranch}/${pageId}/${objBranch}`).on("value", snapshot => {
       const customers = snapshot.val()
       _(`[db] [page-${pageId}] customers`, customers)
-      if(!customers) return
-      this.setState({customers: Object.values(customers)})
+      if (!customers) return
+      this.setState({ customers: Object.values(customers) })
     })
   }
 
   storePageToken = () => {
-    const {db} = this.state
-    if(!db) return _("[storePageToken] No db to run")
-    const {page : {id: pageId, access_token: pageToken}} = this.props
+    const { db } = this.state
+    if (!db) return _("[storePageToken] No db to run")
+    const { page: { id: pageId, access_token: pageToken } } = this.props
     db.ref(`${mainBranch}/${pageId}/token`).set(pageToken)
   }
 
   storeMsg = e => {
     const msg = e.target.value
     _("[msg]", msg)
-    this.setState({msg})
+    this.setState({ msg })
   }
 
   isSelected = userId => {
-    const {selectedCustomers} = this.state
+    const { selectedCustomers } = this.state
     return Boolean(selectedCustomers.filter(id => id === userId)[0])
   }
 
   toggleAddToSendList = userId => {
-    const {selectedCustomers: curr} = this.state
+    const { selectedCustomers: curr } = this.state
     const selected = this.isSelected(userId)
     const filteredList = curr.filter(id => id !== userId)
     const selectedCustomers = !selected ? [...curr, userId] : filteredList
     _("selectedCustomers", selectedCustomers)
-    this.setState({selectedCustomers})
+    this.setState({ selectedCustomers })
   }
 
   broadcastMsg = () => {
     _("[broadcastMsg] Sending...")
-    this.setState({isSending: true})
-    const {selectedCustomers, msg: text} = this.state
-    const {page: {id: pageId, access_token: pageToken} = {}} = this.props
+    this.setState({ isSending: true })
+    const { selectedCustomers, msg: text } = this.state
+    const { page: { id: pageId, access_token: pageToken } = {} } = this.props
     const noCustomerSelected = selectedCustomers.length === 0
 
-    if(noCustomerSelected) {
+    if (noCustomerSelected) {
       window.confirm("Please pick at least one customer.")
-      this.setState({isSending: false})
+      this.setState({ isSending: false })
       return
     }
 
     const waitSendList = selectedCustomers.map(recipientId => {
-      return sendMsg({pageId, pageToken})({text, recipientId}).catch(err => {
-        _("[sendMsg][ERR]Fail at recipientId:",recipientId)
+      return sendMsg({ pageId, pageToken })({ text, recipientId }).catch(err => {
+        _("[sendMsg][ERR]Fail at recipientId:", recipientId)
         _("[sendMsg][ERR]", err)
       })
     })
@@ -108,7 +111,7 @@ export default class SendMessage extends PureComponent {
         _("[broadcastMsg] Finished")
         this.setState({
           isSending: false, // Reset send status
-          msg: "", // Reset msg fill
+          msg: "" // Reset msg fill
         })
       })
   }
@@ -116,44 +119,54 @@ export default class SendMessage extends PureComponent {
   storePostMsg = e => {
     const postMsg = e.target.value
     _("[postMsg]", postMsg)
-    this.setState({postMsg})
+    this.setState({ postMsg })
   }
 
   storePostUrl = e => {
     const postUrl = e.target.value
     _("[postUrl]", postUrl)
-    this.setState({postUrl})
+    this.setState({ postUrl })
   }
 
   storePostImgFile = e => {
     const postImgFile = e.target.value
     _("[postImgFile]", postImgFile)
-    this.setState({postImgFile})
+    this.setState({ postImgFile })
   }
 
   publishPostX = async () => {
     _("[publishPostX] Publishing...")
-    this.setState({isPosting: true})
-    const {postMsg, postUrl: url} = this.state
-    const {page: {id: pageId, access_token: pageToken} = {}} = this.props
+    this.setState({ isPosting: true })
+    const { postMsg, postUrl: url } = this.state
+    const { page: { id: pageId, access_token: pageToken } = {} } = this.props
 
     const imgId = `imgPage${pageId}`
     const imgFile = window[imgId].files[0]
 
-    await publishPost({pageId, pageToken})({text: postMsg, url, imgFile})
+    const success = await publishPost({ pageId, pageToken })({ text: postMsg, url, imgFile })
     _("[publishPostX] Finished")
+
+    if (!success) {
+      this.setState({
+        isPosting: false
+      })
+      return
+    }
+
     this.setState({
       isPosting: false, // Reset send status
       postMsg: "", // Reset msg fill
+      postUrl: "",
+      postImgFile: ""
     })
   }
 
   render() {
-    const {page: {name} = {}} = this.props
-    const {page: {id: pageId, access_token: pageToken} = {}} = this.props
-    const {customers, isSending, msg, isPosting, postMsg} = this.state
+    const { page: { name } = {} } = this.props
+    const { page: { id: pageId, access_token: pageToken } = {} } = this.props
+    const { customers, isSending, msg, isPosting, postMsg, postUrl, postImgFile } = this.state
     const sendBtnTxt = isSending ? "Sending..." : "Send"
-    const postBtnTxt = isPosting ? "Publishing..." :"Publish"
+    const postBtnTxt = isPosting ? "Publishing..." : "Publish"
     const hasCustomer = customers && customers.length > 0
 
     return (
@@ -164,52 +177,50 @@ export default class SendMessage extends PureComponent {
             style={s.textAreaPost}
             placeholder={"Your Post Message"}
             onChange={this.storePostMsg}
-            value={postMsg}/>
+            value={postMsg}
+          />
           <div style={s.imgContainerDiv}>
             <input
               type={"file"}
               style={s.inputImg}
               id={`imgPage${pageId}`}
+              value={postImgFile}
               onChange={this.storePostImgFile}
             />
             <input
               type={"text"}
               style={s.inputTxt}
               onChange={this.storePostUrl}
+              value={postUrl}
               placeholder={"Paste photo's link"}
             />
-            <div className={"postBtn"}
-                 style={s.postDivBtn}
-                 onClick={this.publishPostX}>
+            <div className={"postBtn"} style={s.postDivBtn} onClick={this.publishPostX}>
               {postBtnTxt}
             </div>
           </div>
         </div>
         <div>{pageId}</div>
         <div>{pageToken}</div>
-        {hasCustomer && <div style={s.msgContainerDiv}>
-          <div style={s.msgDiv}>
-            {customers.map(userInfo =>
-              <CustomerInfo
-                key={userInfo.id}
-                userInfo={userInfo}
-                selectUser={this.toggleAddToSendList}
-                selected={this.isSelected(userInfo.id)}/>)}
-          </div>
-          <div style={s.sendCmdContainerDiv}>
-            <textarea
-              style={s.textArea}
-              placeholder={"Your Message"}
-              onChange={this.storeMsg}
-              value={msg}/>
-            <div
-              className={"fb-message-blue"}
-              style={s.sendDivBtn}
-              onClick={this.broadcastMsg}>
-              <i/> {sendBtnTxt}
+        {hasCustomer && (
+          <div style={s.msgContainerDiv}>
+            <div style={s.msgDiv}>
+              {customers.map(userInfo => (
+                <CustomerInfo
+                  key={userInfo.id}
+                  userInfo={userInfo}
+                  selectUser={this.toggleAddToSendList}
+                  selected={this.isSelected(userInfo.id)}
+                />
+              ))}
+            </div>
+            <div style={s.sendCmdContainerDiv}>
+              <textarea style={s.textArea} placeholder={"Your Message"} onChange={this.storeMsg} value={msg} />
+              <div className={"fb-message-blue"} style={s.sendDivBtn} onClick={this.broadcastMsg}>
+                <i /> {sendBtnTxt}
+              </div>
             </div>
           </div>
-        </div>}
+        )}
       </div>
     )
   }
